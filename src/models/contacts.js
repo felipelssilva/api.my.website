3090a28f8721edcbd831338426dbeb56568a4434
Felipe Luis <felipeluis.6@gmail.com>
2022-04-03 18:14:36 -0300

    :rocket:

---
+++ src/models/contacts.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const { timeStamp } = require('console');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Contacts', schema);
