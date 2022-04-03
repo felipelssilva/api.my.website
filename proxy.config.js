c62c55b1ba4d6b960e05da592286cdc5c3f7d385
Felipe Luis <felipeluis.6@gmail.com>
2022-04-03 19:22:01 -0300

    update proxy config

---
+++ proxy.config.js
@@ -0,0 +1,10 @@
+const proxy = [
+    {
+        context: '/api',
+        // target: 'http://localhost:3000/api',
+        target: 'https://quiet-harbor-71572.herokuapp.com/api',
+        pathRewrite: { '^/api': '' }
+    }
+];
+
+module.exports = proxy;
