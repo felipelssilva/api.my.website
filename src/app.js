fca9c5617f7fffcd38a987e9121af5856f296a04
Felipe Luis <felipeluis.6@gmail.com>
2022-04-17 16:43:39 -0300

    fix: cors headers

--- src/app.js
+++ src/app.js
@@ -56,7 +56,8 @@ app.use((req, res, next) => {
     }
     
     res.setHeader('Access-Control-Allow-Methods', 'GET');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
     res.header('Access-Control-Allow-Credentials', true);
     next();
 });
